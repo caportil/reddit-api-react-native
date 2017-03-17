23b78ec995dbbed0269c000f08f934362dca592b
caportil <caportil@gmail.com>
2017-03-17 03:02:36 -0700

    Extract object info from fetch and add to store

--- App.js
+++ App.js
@@ -19,7 +19,25 @@ const store = createStore(reducers);
 // store.subscribe(() => console.log('store has been changed; new store:', store.getState()));
 
 axios.get('https://www.reddit.com/.json').then(res => {
-  res.data.data.children.forEach(child => store.dispatch({type: 'ADD_LIST', payload: child.data.title}))
+  res.data.data.children.forEach(child => store.dispatch({type: 'ADD_LIST', payload: {
+    title: child.data.title,
+    author: child.data.author,
+    created: child.data.created,
+    domain: child.data.domain,
+    id: child.data.id,
+    name: child.data.name,
+    num_comments: child.data.num_comments,
+    permalink: child.data.permalink,
+    // preview: child.data.preview,
+    score: child.data.score,
+    subreddit: child.data.subreddit,
+    subreddit_id: child.data.subreddit_id,
+    subreddit_name_prefixed: child.data.subreddit_name_prefixed,
+    thumbnail: child.data.thumbnail,
+    ups: child.data.ups,
+    url: child.data.url,
+    visited: child.data.visited
+  }}))
 })
   .then(res => console.log('Finished running axios.get; store:', store.getState()));
 
