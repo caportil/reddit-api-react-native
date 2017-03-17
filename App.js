8d9e29122d7119495d07076ac0fb1a5211f1f92c
caportil <caportil@gmail.com>
2017-03-17 02:49:37 -0700

    Setup basic reducer + store, fetch and log default data

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import axios from 'axios';
+
+const listReducer = (state = [], action) => {
+  if (action.type === 'ADD_LIST') {
+    state = state.concat([action.payload])
+  }
+  return state;
+};
+
+const reducers = combineReducers({
+  lists: listReducer,
+});
+
+const store = createStore(reducers);
+
+// store.subscribe(() => console.log('store has been changed; new store:', store.getState()));
+
+axios.get('https://www.reddit.com/.json').then(res => {
+  res.data.data.children.forEach(child => store.dispatch({type: 'ADD_LIST', payload: child.data.title}))
+})
+  .then(res => console.log('Finished running axios.get; store:', store.getState()));
 
 export default class App extends React.Component {
   render() {
+    console.log('Test log');
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>Reddit API React Native!</Text>
+        <Text>Fetched Lists Below:</Text>
       </View>
     );
   }
