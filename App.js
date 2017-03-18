88a04dd669f0c35f808f1b928784d80b9af61fdf
caportil <caportil@gmail.com>
2017-03-18 14:47:36 -0700

    Integrate Navigator and set initialRoute to Home

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Navigator } from 'react-native';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import axios from 'axios';
 import { connect, Provider } from 'react-redux';
 import Home from './Home';
+import ListView from './ListView';
 import thunk from 'redux-thunk';
 // import store from './Store'
 
@@ -32,13 +33,24 @@ const store = createStore(reducers, middleware);
   // .then(res => console.log('Finished running axios.get; store.lists[0]:', store.getState().lists[0]));
 
 export default class App extends React.Component {
+  renderScene(route,nav) {
+    switch (route.screen) {
+      case "Home":
+        return <Home navigator={nav} />
+      case "ListView":
+        return <ListView navigator={nav} />
+      }
+  }
 
   render() {
     let self = this;
     console.log('Test logz');
     return (
       <Provider store={store}>
-        <Home />
+        <Navigator
+          initialRoute={{screen: 'Home'}}
+          renderScene={(route, nav) => this.renderScene(route, nav)}
+        />
       </Provider>
     );
   }
