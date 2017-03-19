9b091143d3c09c00537cc7f145643547fb067484
caportil <caportil@gmail.com>
2017-03-18 22:58:06 -0700

    Combine Text components into single entity in ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,15 +5,13 @@ export default props => {
   return (
     <View>
       <Text>{`${props.idx+1}: ${props.title}`}</Text>
+
       {props.thumbnail.slice(0,4) !== 'http' ?
         <Image source={{uri: 'http://i-cdn.phonearena.com/images/article/70867-image/Best-Reddit-clients-for-Android.jpg'}} style={{height: 50, width: 50}}/>
         :
-        <Image source={{uri: props.thumbnail}} style={{height: 50, width: 50}}/>
-      }
-      <Text>{`upvotes: ${props.ups}\ncomments: ${props.num_comments}`}</Text>
-      <Text>{`submitted by ${props.author} to ${props.subreddit_name_prefixed}`}</Text>
-      <Text>{`${new Date(props.created)}`}</Text>
-      <Text>{`\n`}</Text>
+        <Image source={{uri: props.thumbnail}} style={{height: 50, width: 50}}/>}
+
+      <Text>{`upvotes: ${props.ups}\ncomments: ${props.num_comments}\nsubmitted by ${props.author} to ${props.subreddit_name_prefixed}\n${new Date(props.created)}\n`}</Text>
     </View>
   )
 }
