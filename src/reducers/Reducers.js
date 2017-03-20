d561292f58aac9d8d1d7b9d090347055f08de040
caportil <caportil@gmail.com>
2017-03-19 20:40:33 -0700

    Update listsReducer to overwrite previous listings on dispatch

--- src/reducers/Reducers.js
+++ src/reducers/Reducers.js
@@ -3,7 +3,8 @@ import {persistStore, autoRehydrate} from 'redux-persist';
 
 const listReducer = (state = [], action) => {
   if (action.type === 'ADD_LIST') {
-    state = state.concat([action.payload])
+    let index = action.index;
+    state = state.slice(0, index).concat([action.payload]).concat(state.slice(index + 1));
   }
   return state;
 };
